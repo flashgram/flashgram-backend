8cf175be6a08ead8640ef4842d503b0909f589cf
JinyWiny <jiny95.dev@gmail.com>
2025-02-03 21:12:14 +0900

    feat(user): 유저 도메인, 유저 정보 및 팔로우/언팔로우 관리 로직 구현
    - User 도메인 생성: 유저 정보 및 팔로잉, 팔로워 수를 관리
    - UserInfo: 유저의 기본 정보 관리 (이름, 프로필 이미지)
    - UserRelationCounter: 팔로잉, 팔로워 수를 관리하는 카운터 클래스 추가
    - 팔로우/언팔로우 기능 구현: 자신의 팔로잉 수와 대상 유저의 팔로워 수 변경
    - 예외 처리: 자기 자신을 팔로우/언팔로우 할 수 없음

---
+++ src/main/java/com/app/flashgram/user/domain/UserRelationCounter.java
@@ -0,0 +1,21 @@
+package com.app.flashgram.user.domain;
+
+public class UserRelationCounter {
+
+    private int count;
+
+    public UserRelationCounter() {
+        this.count = 0;
+    }
+
+    public void increase() {
+        this.count++;
+    }
+
+    public void decrease() {
+        if (this.count <= 0) {
+            return;
+        }
+        this.count--;
+    }
+}
