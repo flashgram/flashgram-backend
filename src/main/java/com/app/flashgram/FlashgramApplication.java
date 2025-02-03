4d7ab37ec25334d108697818759a0fd7c059871b
JinyWiny <jiny95.dev@gmail.com>
2025-02-03 19:04:17 +0900

    Initial commit

---
+++ src/main/java/com/app/flashgram/FlashgramApplication.java
@@ -0,0 +1,13 @@
+package com.app.flashgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashgramApplication.class, args);
+	}
+
+}
